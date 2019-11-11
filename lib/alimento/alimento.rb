388f713ab50426cb8c29b7b86902fb2e671edd66
Hernan Gonzalez <alu0101050218@ull.edu.es>
2019-11-11 12:05:04 +0000

    Sextas expectativas conseguidas (ACIERTO -> terreno)

--- lib/alimento/alimento.rb
+++ lib/alimento/alimento.rb
@@ -2,10 +2,10 @@ module Alimento
 
 	class Alimento
 		
-		attr_reader :nombre, :proteinas, :carbohidratos, :lipidos, :gei
+		attr_reader :nombre, :proteinas, :carbohidratos, :lipidos, :gei, :terreno
 
-		def initialize (nombre, proteinas, carbohidratos, lipidos, gei)
-			@nombre, @proteinas, @carbohidratos, @lipidos, @gei  = nombre, proteinas, carbohidratos, lipidos, gei
+		def initialize (nombre, proteinas, carbohidratos, lipidos, gei, terreno)
+			@nombre, @proteinas, @carbohidratos, @lipidos, @gei, @terreno  = nombre, proteinas, carbohidratos, lipidos, gei, terreno
 		end
 
 	end
