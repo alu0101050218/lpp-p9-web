62691e7270e42f86c66016d09fb548b8cccfa8b5
Hernan Gonzalez <alu0101050218@ull.edu.es>
2019-11-18 20:12:54 +0000

    Conseguido introducir nodos en la lista (ACIERTO)

--- lib/alimento/list.rb
+++ lib/alimento/list.rb
@@ -11,7 +11,13 @@ module Alimento
 			@tail = @head
 		end
 
-		def insert_
+		def insert_head (value)
+			actual_head = @head
+			new_head = Node.new(value, nil,actual_head)
+			actual_head.next = new_head
+		end
+
+
 	end
 
 end
