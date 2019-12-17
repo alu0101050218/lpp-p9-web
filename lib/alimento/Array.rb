0096522bb25578c972dad171fb5dbbba3018054a
Hernan Gonzalez <alu0101050218@ull.edu.es>
2019-12-17 21:13:36 +0000

    Documentada clase Array

--- lib/alimento/Array.rb
+++ lib/alimento/Array.rb
@@ -1,9 +1,17 @@
+# @title Modificacion de la clase Array ya existente
+# @author Hernan Daniel Gonzalez Guanipa
 class Array
 
+
+	# @note Calculo del elemento maximo del array
+	# @return [Object] Objeto del mayor dato entre los comparados
   def Max
     self.max{|a, b| a.huella_nutricional <=> b.huella_nutricional}
   end
 
+	# @note Aumento de los precios segun el indice
+	# @param precios Array con valores flotantes
+	# @return [Array] Vector con los valores aumentados
   def high_prices(precios)
     huella_maxima = self.Max.huella_nutricional
     i = 0
